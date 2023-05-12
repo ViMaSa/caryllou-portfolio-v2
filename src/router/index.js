fe5fc8361313e5df7d1c7cad2f96deb41f916f54
Victor Sarmiento <sarmiv07@gmail.com>
2023-05-11 19:22:05 -0700

    Set proper pathing since vue router does not account for repo name on gh pages

--- src/router/index.js
+++ src/router/index.js
@@ -29,6 +29,9 @@ const routes = [
 
 const router = createRouter({
   history: createWebHistory(),
+  base: process.env.NODE_ENV === 'production'
+    ? '/caryllou-portfolio-v2/'
+    : '/',
   routes,
 });
 
